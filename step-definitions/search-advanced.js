883f9308f50d3dbad2e396e226100fd4b6bfa2bc
Paatsu <johnny@wong.se>
2020-05-19 20:44:08 +0200

    2.1 fix

--- step-definitions/search-advanced.js
+++ step-definitions/search-advanced.js
@@ -14,8 +14,10 @@ module.exports = function () {
 
   this.Given(/^have clicked "([^"]*)" beside "([^"]*)"$/, async function (keyword, findText) {
 
-    let findTextOnPage = await driver.wait(until.elementLocated(By.css('.imdb-search-gateway__browse'))).getText();
-    expect(findTextOnPage, 'Could not find the text' + findText + ' on the page').to.include(findText);
+    await driver.wait(until.elementLocated(By.css('.imdb-search-gateway__browse')));
+    let findTextOnPage = await $('.imdb-search-gateway__browse');
+    let findTextOnPageText = await findTextOnPage.getText();
+    expect(findTextOnPageText, 'Could not find the text' + findText + ' on the page').to.include(findText);
     let keywordLink = await driver.findElement(By.linkText('Keyword')).click();
     expect(keywordLink, 'Could not find the link ' + keyword + ' on the page')
 
@@ -23,38 +25,51 @@ module.exports = function () {
 
   this.Given(/^entered the keyword "([^"]*)" in the input field beside "([^"]*)" and clicked "([^"]*)"$/, async function (keyword, findText, button) {
 
-    let findTextOnPage = await driver.wait(until.elementLocated(By.css('.searchbox'))).getText();
-    expect(findTextOnPage, 'Could not find the text' + findText + ' on the page').to.include(findText);
-    let inputField = await driver.wait(until.elementLocated(By.css("form.searchbox input[name='q']")));
+    await driver.wait(until.elementLocated(By.css('.searchbox')));
+    let findTextOnPage = await $('.searchbox');
+    let findTextOnPageText = await findTextOnPage.getText();
+    expect(findTextOnPageText, 'Could not find the text' + findText + ' on the page').to.include(findText);
+    await driver.wait(until.elementLocated(By.css("form.searchbox input[name='q']")));
+    let inputField = await $("form.searchbox input[name='q']");
     expect(inputField, 'Could not find the input field on the page');
     await inputField.sendKeys(keyword);
-    let searchButton = await driver.wait(until.elementLocated(By.css('input.btn'))).click();
+    await driver.wait(until.elementLocated(By.css('input.btn')));
+    let searchButton = await $('input.btn');
     expect(searchButton, 'Could not find the ' + button + ' button');
+    await searchButton.click();
 
   });
 
   this.Given(/^clicked "([^"]*)" on the "([^"]*)" "([^"]*)" page$/, async function (link, findText, keyword) {
 
     keywordOnPage = link;
-    let findTextOnPage = await driver.wait(until.elementLocated(By.css('h1.findHeader'))).getText();
-    expect(findTextOnPage, 'Could not find the text ' + findText + ' ' + keyword + ' on the page').to.include(findText, keyword);
-    let linkOnPage = await driver.findElement(By.linkText(link)).click();
+    await driver.wait(until.elementLocated(By.css('h1.findHeader')));
+    let findTextOnPage = await $('h1.findHeader');
+    let findTextOnPageText = await findTextOnPage.getText();
+    expect(findTextOnPageText, 'Could not find the text ' + findText + ' ' + keyword + ' on the page').to.include(findText, keyword);
+    let linkOnPage = await driver.findElement(By.linkText(link));
     expect(linkOnPage, 'Could not find ' + link + ' on the page');
+    await linkOnPage.click();
 
   });
 
   this.Given(/^the "([^"]*)" has loaded$/, async function (h1Text) {
 
-    let header = await driver.wait(until.elementLocated(By.css('h1.header'))).getText();
-    expect(header, 'Could not find the header' + h1Text + ' on the page').to.include(h1Text);
+    await driver.wait(until.elementLocated(By.css('h1.header')));
+    let header = await $('h1.header');
+    let headerText = await header.getText();
+    expect(headerText, 'Could not find the header' + h1Text + ' on the page').to.include(h1Text);
 
   });
 
   this.Given(/^the "([^"]*)" corresponding to selected keyword is checked in the "([^"]*)" options \(filter\)$/, async function (keyword, refineFilter) {
 
-    let findText = await driver.wait(until.elementLocated(By.css('div.faceter-header span.expand strong'))).getText();
-    expect(findText, 'Could not find the filter option').to.include(refineFilter);
-    let keywordChecked = await driver.wait(until.elementLocated(By.css('.faceter-fieldset input[name="' + keywordOnPage + '"][checked="checked"]')))
+    await driver.wait(until.elementLocated(By.css('div.faceter-header span.expand strong')));
+    let findText = await $('div.faceter-header span.expand strong');
+    let findTextText = await findText.getText();
+    expect(findTextText, 'Could not find the filter option').to.include(refineFilter);
+    driver.wait(until.elementLocated(By.css('.faceter-fieldset input[name="' + keywordOnPage + '"][checked="checked"]')));
+    let keywordChecked = await $('.faceter-fieldset input[name="' + keywordOnPage + '"][checked="checked"]');
     expect(keywordChecked, 'Could not find the right ' + keyword + ' to be checked');
 
   });
@@ -67,7 +82,7 @@ module.exports = function () {
 
   this.Then(/^"([^"]*)" should be among the top (\d+) results on that page$/, async function (keyword, toplist) {
 
-    let getSearchResult = await driver.findElements(by.css('h3.lister-item-header a'));
+    let getSearchResult = await $('h3.lister-item-header a');
     expect(getSearchResult.length, 'Could not find any results').to.equal(+toplist);
 
     let found = false;
